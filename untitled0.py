7f405cf0a5ab8c252064217c35aa7f8200a0ff30
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-12 01:21:39 +0530

    feeling sleepy.rest tomorrow

--- untitled0.py
+++ untitled0.py
@@ -8,5 +8,8 @@ Created on Sat Jan 12 01:06:10 2019
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import warnings 
+import warnings
+warnings.filterwarnings('ignore')
+dataset = pd.read_csv("ml-100k/u.data",sep='\t',names="user_id,item_id,rating,timestamp".split(","))
+print(dataset.head(5)) 
 
