2ffef0c804f1352554b514388c3b6bd4ed33374b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-12 21:10:00 +0530

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -29,5 +29,25 @@ we want to estimate low dimensional matrices (W [M X k] and H [M X k]), such tha
 '''
 
 import keras
+from keras.layers import merge
 from IPython.display import SVG
 from keras.optimizers import Adam
+from keras.utils.vis_utils import model_to_dot
+n_users, n_movies = len(dataset.user_id.unique()), len(dataset.item_id.unique())
+n_latent_factors = 3
+'''
+next  I am going to create an embedding for movies and users and combine them into a dot product
+the dot product is taken for a certain movie and the user rating .
+'''
+movie_input = keras.layers.Input(shape=[1],name='Item')
+movie_embedding = keras.layers.Embedding(n_movies + 1, n_latent_factors, name='Movie-Embedding')(movie_input)
+movie_vec = keras.layers.Flatten(name='FlattenMovies')(movie_embedding)
+
+user_input = keras.layers.Input(shape=[1],name='User')
+user_vec = keras.layers.Flatten(name='FlattenUsers')(keras.layers.Embedding(n_users + 1, n_latent_factors,name='User-Embedding')(user_input))
+added = keras.layers.add()([movie_vec, user_vec])
+
+
+prod = keras.layers.merge([movie_vec, user_vec], mode='dot',name='DotProduct')
+model = keras.Model([user_input, movie_input], prod)
+model.compile('adam', 'mean_squared_error')
