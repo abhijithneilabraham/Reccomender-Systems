42606f8edd79be5f1454c4dba1211510cbfd27b3
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-25 03:01:49 +0530

    Yes!completed

--- customer.py
+++ customer.py
@@ -22,20 +22,20 @@ print(get_id(m))
 '''
 rating=[]
 
-def get_rating():
+
+'''
+print('enter the ratings in range 1 to 10')#Let this print statement stay here for future uses when I dont use random numbers
+'''
+for i in range(10):
     '''
-    print('enter the ratings in range 1 to 10')#Let this print statement stay here for future uses when I dont use random numbers
+    print('enter the rating for question%d'%(i))
     '''
-    for i in range(10):
-        '''
-        print('enter the rating for question%d'%(i))
-        '''
-        a=random.randint(1,10) #generating random values for ratings for testing purposes
-        if a<=10 and a>0:           
-            rating.append(a)
-        else:
-            raise Exception('invalid rating.enter a value between 1 and 10')
-    return rating
+    a=random.randint(1,10) #generating random values for ratings for testing purposes
+    if a<=10 and a>0:           
+        rating.append(a)
+    else:
+        raise Exception('invalid rating.enter a value between 1 and 10')
+
 '''
 print(get_rating())
 '''
@@ -47,15 +47,32 @@ mf=[]
 rat=[]
 id_cust=[]
 for customer in range(m):
-    id_cust.append(id[customer]) #for getting array of customer id
-    rat.append(get_rating()) #for getting a full array of ratings with number of elements=10*number of customers
-    mf.append(random.randint(0,1)) #male or female random generator
-details=[id_cust,rat[0],mf]
+    id_cust.append([id[customer]]*10) #for getting array of customer id.mulitplied by 10 to match with the column of ratings
+    
+    rat.append(rating) #for getting a full array of ratings with number of elements=10*number of customers
+    mf.append([random.randint(0,1)]*10) #male or female random generator
+print(id_cust,mf,rat)
+
 import csv
+'''
 with open("customerdataset.csv","w+") as my_csv:
     csvWriter=csv.writer(my_csv,delimiter=',')
     csvWriter.writerows(details)
-        
-        
+
+'''
+
+with open('names.csv', 'w') as csvfile:
+    fieldnames = ['id', 'rating','male_or_female']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for i in range(m):
+        for j in range(10):
+            writer.writerow({'id': id_cust[i][j], 'rating':rat[i][j],'male_or_female':mf[i][j]})
+'''
+wtr = csv.writer(open ('out.csv', 'w'), delimiter=',', lineterminator='\n')
+for x in id_cust : wtr.writerow ([x])   
+for x in id_cust : wtr.writerow ([x])  
+'''
         
     
