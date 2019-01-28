ff223b3eeddee9d5d304069be5b84df70ef1e9f3
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-01-29 02:14:03 +0530

    seems bad

--- customer.py
+++ customer.py
@@ -21,12 +21,12 @@ def get_id(n):
 print(get_id(m))  
 '''
 rating=[]
-
+r=int(input('enter the number of ratings'))
 
 '''
 print('enter the ratings in range 1 to 10')#Let this print statement stay here for future uses when I dont use random numbers
 '''
-for i in range(10):
+for i in range(r):
     '''
     print('enter the rating for question%d'%(i))
     '''
@@ -47,7 +47,7 @@ mf=[]
 rat=[]
 id_cust=[]
 for customer in range(m):
-    id_cust.append([id[customer]]*10) #for getting array of customer id.mulitplied by 10 to match with the column of ratings
+     #for getting array of customer id.mulitplied by 10 to match with the column of ratings
     
     rat.append(rating) #for getting a full array of ratings with number of elements=10*number of customers
     mf.append([random.randint(0,1)]*10) #male or female random generator
@@ -61,9 +61,9 @@ with open("customerdataset.csv","w+") as my_csv:
 
 '''
 que=[]
-for i in range(10):
-    que.append('rating'+str(i))
-print(que)    
+for i in range(r):
+    que.append('rating'+str(i+1))
+    
 
 with open('names.csv', 'w') as csvfile:
     fieldnames = ['id', 'rating','male_or_female']
@@ -71,8 +71,8 @@ with open('names.csv', 'w') as csvfile:
 
     writer.writeheader()
     for i in range(m):
-        for j in range(10):
-            writer.writerow({'id': id_cust[i][j], 'rating':rat[i][j],'male_or_female':mf[i][j]})
+        
+        writer.writerow({'id': id_cust[i], 'rating':rat[i],'male_or_female':mf[i]})
 '''
 wtr = csv.writer(open ('out.csv', 'w'), delimiter=',', lineterminator='\n')
 for x in id_cust : wtr.writerow ([x])   
