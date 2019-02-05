d560ee176c596c4c27be556c4333cb4ba4743b25
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-02-05 14:18:23 +0530

    hmm its a go

--- Recommendation.py
+++ Recommendation.py
@@ -14,13 +14,14 @@ import pandas as pd
 customer=pd.read_csv('names.csv')
 
 reader = Reader(line_format='user item rating',rating_scale=(1, 5),sep=',')
-fieldnames = ['id', 'male_or_female','question1']
-'''
+fieldnames = ['id', 'male_or_female']
+
 for i in range(25):
     fieldnames.insert(2,'question'+str(i+1))
-'''
-data = Dataset.load_from_df(customer[fieldnames], reader)
 
-data=Dataset.load_from_file('names.csv',reader=reader)
-algo=SVD() #using the SVD algorithm
-cross_validate(algo,data,measures=['RMSE','MAE'],cv=5,verbose=True)
+    data = Dataset.load_from_df(customer[fieldnames], reader)
+    del fieldnames[2]
+    
+   
+    algo=SVD() #using the SVD algorithm
+    cross_validate(algo,data,measures=['RMSE','MAE'],cv=5,verbose=True)
